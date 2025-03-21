9c791d94dce33eacac46425972ad9ea895518dfd
Nithish <nithishsunny1234@gmail.com>
2025-03-21 17:28:00 +0530

    final commit

--- Modify/script.js
+++ Modify/script.js
@@ -1,3 +1,8 @@
-function handleClick() {
-  console.log("first");
-}
+let icon = document.getElementsByClassName("fa-solid");
+let icon_div = document.getElementsByClassName("nav_slider");
+
+icon[0].addEventListener("click", () => {
+  icon[0].classList.toggle("fa-xmark");
+  let has = icon[0].classList.contains("fa-xmark");
+  has ? (icon_div[0].style.left = "0px") : (icon_div[0].style.left = "-227px");
+});
